bfbfb48ff041a0ed806b3e4e4ca1daaab438d62f
刘淑文 <LancasterLiu_cryp@163.com>
2023-03-18 15:29:06 +0800

    estiblish the structure

--- mindnlp/models/roberta/__init__.py
+++ mindnlp/models/roberta/__init__.py
@@ -1,3 +1,14 @@
 # __init__.py
 # LancasterLiu
 # 2023/3/8
+
+"""
+Roberta Model.
+"""
+from mindnlp.models.roberta import roberta, roberta_config
+from mindnlp.models.roberta.roberta import *
+from mindnlp.models.roberta.roberta_config import *
+
+__all__ = []
+__all__.extend(roberta.__all__)
+__all__.extend(roberta_config.__all__)
--- mindnlp/models/roberta/roberta.py
+++ mindnlp/models/roberta/roberta.py
@@ -1,3 +1,19 @@
 # model
 # LancasterLiu
 # 2023/3/8
+"""MindNLP Roberta model"""
+import mindspore.numpy as mnp
+import mindspore.common.dtype as mstype
+from mindspore import nn
+from mindspore import ops
+from mindspore import Parameter, Tensor
+from mindspore.common.initializer import initializer, TruncatedNormal
+from mindnlp._legacy.nn import Dropout
+
+
+""" 
+__all__ = [
+     'RobertaEmbeddings', 'RobertaAttention', 'RobertaEncoder', 'RobertaIntermediate', 'RobertaLayer',
+    'RobertaModel', 'RobertaForPretraining', 'RobertaLMPredictionHead'
+]
+"""
